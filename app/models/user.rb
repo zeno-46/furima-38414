f51c82dd584dc4662b62a379f428cbf808a643a4
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-09-16 10:40:36 +0900

    新規登録、ログイン、エラー

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+    validates :nickname, presence: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :first_name_kana, presence: true
+    validates :last_name_kana, presence: true
+    validates :birthday, presence: true
+    
+    
+    has_many :purchases
+    has_many :items
+
+end
