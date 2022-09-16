665d672a2f1f434e310714747f6b58a38d96277b
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-09-16 19:03:47 +0900

    7行目のunique: trueを削除

--- db/migrate/20220915080740_devise_create_users.rb
+++ db/migrate/20220915080740_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, unique: true, default: ""
+      t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :nickname,           null: false
       t.string :first_name,         null: false
