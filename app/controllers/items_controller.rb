7282f2477c01ddb708f1a55cdc0fe4774359ee15
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-09-17 17:53:04 +0900

    itemモデルの作成、Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,9 @@ class ItemsController < ApplicationController
   #def create
   #end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+
 end
