459e9dca0409eb64b468533b8151d251cc7cfdef
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-09-18 23:05:11 +0900

    商品出品ページを表示できるようにする、dateからdataに一部訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,15 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :schedeled_delivery
+  
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :delivery_fee_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :scheduled_delivery_id, numericality: {other_than: 1 }
 end
