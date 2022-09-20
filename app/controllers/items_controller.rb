4d21f7ae1f29a34ddd2cefed1c647047427bd4ee
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-09-20 15:08:55 +0900

    @items = Item.allのコメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    @items = Item.all
+    #@items = Item.all
   end
 
   def new
