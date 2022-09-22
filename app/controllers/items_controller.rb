904a88e05c20af6bdd518b8f90d2ebdb7722663e
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-09-22 21:48:55 +0900

    可読性を上げるため@item = Item.find(params[:id])の記述をまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only:[:edit]
+  before_action :set_item, only:[:show, :edit, :update]
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -19,15 +20,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
       redirect_to action: :show
@@ -40,6 +38,10 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :image, :price, :explanation, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :scheduled_delivery_id).merge(user_id: current_user.id)
+  end
+  
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   def move_to_index
