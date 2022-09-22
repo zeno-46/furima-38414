d0d7c79a61f0c82af8c4125857207ca760f7af43
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-09-22 16:19:01 +0900

    editアクションのルーティング、定義、エラーメッセージ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,13 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+  end
+
   private
 
   def item_params
