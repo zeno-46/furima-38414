08c35b84a228cce30ef32542eb870b94d6118425
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-09-23 16:16:28 +0900

    Revert "Revert "＠のつけ忘れ、destroyアクション定義""
    This reverts commit ecd4c23c87ac80ededc8587d9a52f6bc4ba86e34.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only:[:show, :edit, :update]
+  before_action :set_item, only:[:show, :edit, :update, :destroy]
   before_action :move_to_index, only:[:edit]
  
   def index
@@ -32,6 +32,13 @@ class ItemsController < ApplicationController
       redirect_to action: :show
     else
       render action: :edit
+    end
+  end
+
+  def destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
     end
   end
 
