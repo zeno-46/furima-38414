bf5fa98a2ecff869288eee5e614c50dd461ebf2c
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-09-23 16:52:12 +0900

    else削除、move_to_indexにdestroy追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only:[:show, :edit, :update, :destroy]
-  before_action :move_to_index, only:[:edit]
+  before_action :move_to_index, only:[:edit, :destroy]
  
   def index
     @items = Item.all.order(created_at: :desc)
@@ -39,8 +39,6 @@ class ItemsController < ApplicationController
     if @item.user_id == current_user.id
       @item.destroy
       redirect_to root_path
-    else
-      redirect_to edit_item_path
     end
   end
 
