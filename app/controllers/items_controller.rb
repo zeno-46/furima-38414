8988bd80e67450175cb3fdff01cef95bb9864427
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-09-23 16:38:14 +0900

    自身が出品した商品のみが削除できるようにするための条件分岐が付与

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,6 +39,8 @@ class ItemsController < ApplicationController
     if @item.user_id == current_user.id
       @item.destroy
       redirect_to root_path
+    else
+      redirect_to edit_item_path
     end
   end
 
