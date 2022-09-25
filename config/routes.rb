acf7f291764466195bf9aec2818b6b28a2ef9c6b
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-09-25 16:04:39 +0900

    テーブル作成、purchaseコントローラー作成、ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items
+  
+  resources :items do
+  resources :purchases, only:[:index, :create]
+  end
 end
