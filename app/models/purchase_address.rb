ad987fe4fd12855bb2b303970679fd631de56444
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-09-25 14:44:00 +0900

    address,purchaseモデルの作成、フォームオブジェクト作成

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,17 @@
+class PurchaseAdress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :tel, :token
+
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 }
+  validates :city, presence: true
+  validates :house_number, presence: true
+  validates :building_name, presence: true
+  validates :tel, presence: true
+  validates :token, presence: true
+  
+  def save
+    purchase = Purchase.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, tel: tel, purchase_id: purchase.id)
