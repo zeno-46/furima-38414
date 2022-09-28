3719a6cdef72d5e59635ee67d88208dbf225a57e
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-09-28 14:21:26 +0900

    presence: trueは「with_options」でまとめる

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -2,14 +2,16 @@ class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :tel, :token
 
-  validates :user_id, presence: true
-  validates :item_id, presence: true
-  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
-  validates :prefecture_id, presence: true, numericality: { other_than: 1 }
-  validates :city, presence: true
-  validates :house_number, presence: true
-  validates :tel, presence: true, format: { with: /\A\d{10,11}\z/ }
-  validates :token, presence: true
+  with options presence: true do
+  validates :user_id
+  validates :item_id
+  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :city
+  validates :house_number
+  validates :tel, format: { with: /\A\d{10,11}\z/ }
+  validates :token
+  end
   
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
